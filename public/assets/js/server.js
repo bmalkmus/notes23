32a72f24cd87ddf6bf2a07342f0263331c907d7c
bmalkmus <bmalkmus23@gmail.com>
2020-02-24 21:12:50 -0800

    server css and js files working

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,8 +4,20 @@ let path = require("path");
 let notes = express();
 var PORT = process.env.PORT || 2323;
 
+notes.use(express.urlencoded({ extended: true }));
+notes.use(express.json());
+// notes.use('/static', express.static(path.join(__dirname, 'public')));
 
-notes.get ("*", function (req, res) {
+notes.get ("/public/assets/css/styles.css", function (req, res) {
+    res.sendFile(path.join(__dirname, '../../../public/assets/css/styles.css'));
+});
+
+notes.get ("/public/assets/js/index.js", function (req, res) {
+    res.sendFile(path.join(__dirname, '../../../public/assets/js/index.js'));
+});
+
+
+notes.get ("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
